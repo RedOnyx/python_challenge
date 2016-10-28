9158e2741c8e7252059b292960a2e26c41f2b2ab
Micheal Mac Donnacha <micheal.macdonnacha13@gmail.com>
2016-10-28 15:27:33 +0100

    puzzle 01 complete

--- puzzle_01.py
+++ puzzle_01.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
 """
 Website url http://www.pythonchallenge.com/
+
+Puzzle 01 url: http://www.pythonchallenge.com/pc/def/0.html
+
+Calulate 2 to the power of 38
 """
 
+def calc_2pow38():
+	return pow(2, 38)
+
 if __name__ == "__main__":
-    print("Hello World")
+	num = calc_2pow38()
+	print("Challenge: evaluate 2 to the power of 38.\nAnswer: {}".format(num))
+	print("Next challenge url: http://www.pythonchallenge.com/pc/def/{}.html".format(num))
+
