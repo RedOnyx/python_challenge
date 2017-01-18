f6c6062f13eda5828cae16f3753e7ca3d13c633e
Micheal Mac Donnacha <micheal.macdonnacha13@gmail.com>
2017-01-18 22:21:59 +0000

    Puzzle 02 complete

---
+++ puzzle_00.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3.6
+"""
+Website url http://www.pythonchallenge.com/
+
+Puzzle 00 url: http://www.pythonchallenge.com/pc/def/0.html
+
+Calulate 2 to the power of 38
+"""
+
+def calc_2pow38():
+    return pow(2, 38)
+
+if __name__ == "__main__":
+	num = calc_2pow38()
+	print("Challenge: evaluate 2 to the power of 38.\nAnswer: {}".format(num))
+	print("Next challenge url: http://www.pythonchallenge.com/pc/def/{}.html".format(num))
+
--- puzzle_01.py
+++ puzzle_01.py
@@ -1,17 +1,36 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3.6
 """
-Website url http://www.pythonchallenge.com/
+Python Challenge puzzle 01
+Url: http://www.pythonchallenge.com/pc/def/map.html
 
-Puzzle 01 url: http://www.pythonchallenge.com/pc/def/0.html
-
-Calulate 2 to the power of 38
+Clues
+K -> M
+O -> Q
+E -> G
 """
 
-def calc_2pow38():
-	return pow(2, 38)
+encoded_message = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj. "
+# hint ord('m') - ord('k')
+# chr(97) = 'a'
+
+def decode_message(input):
+	
+	message = input
+	var = ord('m') - ord('k')
+	output = ''
+	exceptions = [' ','\'', '.', '(', ')' ]
+	for character in message:
+		if character in exceptions:
+			output += character
+		elif character == 'z':
+			output += 'b'
+		elif character == 'y':
+			output += 'a'
+		else:
+			new_char = chr(ord(character)+var)
+			output += new_char
+	return output
 
 if __name__ == "__main__":
-	num = calc_2pow38()
-	print("Challenge: evaluate 2 to the power of 38.\nAnswer: {}".format(num))
-	print("Next challenge url: http://www.pythonchallenge.com/pc/def/{}.html".format(num))
-
+	print(decode_message(encoded_message))
+	print("http://www.pythonchallenge.com/pc/def/{}.html".format(decode_message('map')))
